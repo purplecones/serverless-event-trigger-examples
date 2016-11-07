b54d11624c31ea834f5489aa4372647d38b9c2f7
mjoldic <mjoldic@capfundr.com>
2016-11-07 14:39:56 -0500

    added fork me ribben

--- meteor/client/modules/core/components/home.jsx
+++ meteor/client/modules/core/components/home.jsx
@@ -6,7 +6,7 @@ const Home = () => (
   <div className="div ui one column grid container">
     <div className="ui column">
       <h1>AWS Lambda Event Triggers using Serverless</h1>
-      <p>Below are a few working examples of how to use the events to trigger your AWS Lambda functions using Serverless</p>
+      <p>Below are a few working examples of how to use events to trigger your AWS Lambda functions using Serverless</p>
     </div>
     <div className="ui column">
       <Sections />
@@ -14,6 +14,13 @@ const Home = () => (
     <div className="ui column">
       <EventsTable />
     </div>
+    <a href="https://github.com/purplecones/serverless-event-trigger-examples">
+      <img
+        style={{position: 'absolute', top: 0, right: 0, border: 0}}
+        src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
+        alt="Fork me on GitHub"
+        data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"/>
+    </a>
   </div>
 );
 
