e1cb79535c05d8283555f108ee3c291e5f60a555
mjoldic <mjoldic@capfundr.com>
2016-11-07 16:01:30 -0500

    added zIndex for forkMe image so that it is below the Demo link.

--- meteor/client/modules/core/components/home.jsx
+++ meteor/client/modules/core/components/home.jsx
@@ -16,7 +16,7 @@ const Home = () => (
     </div>
     <a href="https://github.com/purplecones/serverless-event-trigger-examples">
       <img
-        style={{position: 'absolute', top: 0, right: 0, border: 0}}
+        style={{position: 'absolute', top: 0, right: 0, border: 0, zIndex: -1}}
         src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
         alt="Fork me on GitHub"
         data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"/>
